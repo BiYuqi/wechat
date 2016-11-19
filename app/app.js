b3e3d69517212088f10379b4fa27c9ac8c1a4f2d
xiaobaibudong <biyuqiwan@163.com>
2016-11-19 18:46:08 +0800

    resolve andriod

--- app/app.js
+++ app/app.js
@@ -5,6 +5,17 @@ angular.module('myApp',[
     'directives'
     ])
     .config(function($stateProvider,$urlRouterProvider){
+		$ionicConfigProvider.platform.ios.tabs.style('standard');
+		$ionicConfigProvider.platform.ios.tabs.position('bottom');
+		$ionicConfigProvider.platform.android.tabs.style('standard');
+		$ionicConfigProvider.platform.android.tabs.position('bottom');
+		$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+		$ionicConfigProvider.platform.android.navBar.alignTitle('center');
+		$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+		$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+		$ionicConfigProvider.platform.ios.views.transition('ios');
+		$ionicConfigProvider.platform.android.views.transition('android');
+		
         $stateProvider
             .state('login',{
                 url:'/login',
